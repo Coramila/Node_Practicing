d0614da5235ef26b44e5cd0cb80ba261c54df28b
Camila Carvalho <carvalho.camiilaa@gmail.com>
2022-12-30 13:32:13 -0300

    Try supervillains

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var superHeroes = require("superheroes");
+var superVillains = require("supervillains");
 
 var myHero = superHeroes.random();
+var myVillain = superVillains.random();
 
 console.log("My hero: " + myHero);
+console.log("My villain: " + myVillain);
